9cee24d0578f493c99ad53290f1b1fcba6a26798
“StevenGalvan” <galvan4739@gmail.com>
2023-01-13 11:15:18 -0600

    WIP scripts.js work  and branches

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business Logic
+function handleRadio(event) {
+  event.preventDefault();
+  const radioSelection = document.querySelector("input[name='hor']:checked").value;
+}
+
+window.addEventListener("load", function() {
+  document.getElementById("radio-form").addEventListener("submit", handleRadio);
+
+if ()
+});
