05c9a300e5d7424f28980d8b4cbde7e7b31f18df
“StevenGalvan” <galvan4739@gmail.com>
2023-01-13 16:39:55 -0600

    ADD css elements to styles.css file

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,11 @@ window.onload = function() {
     const answer5 = parseInt(document.querySelector("input#answer5").value);
   
   if ( answer1 && answer2 && answer3 && answer4 && answer5) {
-    if (answer1 >= 2000 && answer4 === 100) {
+    if (answer1 >= 2000 && answer4 === 101) {
       document.getElementById("ruby").removeAttribute("class");
     } else if (answer2 === 1 && answer5 >100) {
       document.getElementById("python").removeAttribute("class");
-    } else if (answer3 >= 10) {
+    } else if (answer3 >= 10 || answer5 <= 1000000000000 && answer4 === 101) {
     document.getElementById("java").removeAttribute("class");
     } 
     } else {
