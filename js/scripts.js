fdf107013b99756e30cf0792bb6e87a1ff4c8c0d
“StevenGalvan” <galvan4739@gmail.com>
2023-01-16 21:46:25 -0600

    BUG FIX form not populating numbers

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,14 @@ window.onload = function() {
     const answer5 = parseInt(document.querySelector("input#answer5").value);
   
   if ( answer1 && answer2 && answer3 && answer4 && answer5) {
-    if (answer1 <= 2000 && answer4 === 101) {
+    if (answer1 >= 20 && answer4 > 100) {
       document.getElementById("ruby").removeAttribute("class");
-    } else if (answer2 === 1 && answer5 >100) {
+    } else if (answer2 === 1 && answer5 >100 && answer1 <= 19) {
       document.getElementById("python").removeAttribute("class");
-    } else if (answer3 >= 10 || answer5 <= 1000000000000 && answer4 === 101) {
+    } else if (answer3 >= 50 || answer4 <= 100) {
     document.getElementById("java").removeAttribute("class");
+    } else if (answer1 <= 3 || answer2 <= 3 || answer3 <= 50 || answer4 >= 100 || answer5 <= 3) {
+      document.getElementById("python").removeAttribute("class");
     } 
     } else {
         document.getElementById("error-message").removeAttribute("class");
